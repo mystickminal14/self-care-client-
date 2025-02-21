9c6a217f8f0236eb54d0bf15a3c9ff79bb7087c9
charitraa <stharabi9862187405@gmail.com>
2025-02-21 22:37:28 +0545

    added menu bar

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-
+import { FaLeaf, FaSmoking, FaHeart, FaSignOutAlt } from "react-icons/fa";
 const Dashboard = () => {
   return (
-    <div className="flex h-screen">
-      <div className="w-1/2 bg-blue-500 flex items-end">
-        di
-      </div>
-      <div className="w-1/2 bg-green-500"></div>
+    <div className="fixed left-0 top-0 h-full flex flex-col gap-4 p-4 bg-transparent">
+      <SidebarButton icon={<FaLeaf />} />
+      <SidebarButton icon={<FaSmoking />} />
+      <SidebarButton icon={<FaHeart />} />
+      <SidebarButton icon={<FaSignOutAlt />} />
     </div>
   );
 };
 
+const SidebarButton = ({ icon }) => {
+  return (
+    <button className="flex items-center justify-center w-12 h-12 rounded-full bg-white bg-opacity-30 hover:bg-opacity-50 shadow-md">
+      {icon}
+    </button>
+  );
+};
 export default Dashboard;
