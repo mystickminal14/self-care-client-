e7ea61189dcc124496c6128bb5cdd7f6a94776f4
mystick minal <mystick14minal@gmail.com>
2025-02-21 22:50:08 +0545

    done

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import root from '../../assets/plants/1.png'
+import stem from '../../assets/plants/2.png'
+import three from '../../assets/plants/3.png'
+import four from '../../assets/plants/4.png'
+import five from '../../assets/plants/5.png'
+import six from '../../assets/plants/6.png'
 
+import "./dash.css";
 const Dashboard = () => {
   return (
-    <div className="flex h-screen">
-      <div className="w-1/2 bg-blue-500 flex items-end">
-        di
+    <div className="flex imageOne justify-center gap-9 items-end p-9 h-screen">
+      <div className="plant">
+        <img src={root} alt="" />
       </div>
-      <div className="w-1/2 bg-green-500"></div>
+      <div className="plant">
+      <img src={stem} alt="" />
+
+      </div>
+      <div className="plant">
+      <img src={three} alt="" />
+      </div>
+      <div className="plant">
+      <img src={four} alt="" />
+      </div>
+      <div className="plant">
+      <img src={five} alt="" />
+      </div>
+      <div className="plant">
+      <img src={six} alt="" />
+      </div>
     </div>
   );
 };
