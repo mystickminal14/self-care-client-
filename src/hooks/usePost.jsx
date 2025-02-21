d3cda41a03e932b6f4aa5294e291ed9b42f04c9c
mystick minal <mystick14minal@gmail.com>
2025-02-21 15:30:47 +0545

    so initial setup completed

--- src/hooks/usePost.jsx
+++ src/hooks/usePost.jsx
@@ -2,21 +2,21 @@ import axios from "../api/BaseApi";
 import React, { useCallback } from "react";
 
 const usePost = (url, body) => {
-  const {  setData, isLoading, setIsLoading } = useContext(AppContext);
+  const { isLoading, setIsLoading } = useContext(AppContext);
 
-  const save = useCallback(async () => {
+  const save = async () => {
     setIsLoading(true);
     try {
       const response = await axios.post(url, body);
-      setData(response.data);
+      console.log("Registration successfull");
       setIsLoading(false);
     } catch (error) {
       setIsLoading(false);
       console.log(error);
     }
-  }, [url,setIsLoading]);
+  }
 
-  return {save,isLoading};
+  return { save, isLoading };
 };
 
 export default usePost;
