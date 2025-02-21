88f945270d330cbc1c418f412809cd9039b0cf22
mystick minal <mystick14minal@gmail.com>
2025-02-21 22:23:21 +0545

    dashbioiard module started

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div className="flex h-screen">
-    <div className="w-1/2 bg-blue-500"></div>
-    <div className="w-1/2 bg-green-500"></div>
-  </div>
+      <div className="w-1/2 bg-blue-500 flex items-end">
+        di
+      </div>
+      <div className="w-1/2 bg-green-500"></div>
+    </div>
   );
 };
 
