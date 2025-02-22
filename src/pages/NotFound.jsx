4410bddd6d65e25f5d8e62330c8837808f63e25a
charitraa <stharabi9862187405@gmail.com>
2025-02-22 08:46:53 +0545

    added not found pages

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,24 @@
+export default function NotFound() {
+  return (
+    <>
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-blue-800 sm:text-7xl">
+            Page not found
+          </h1>
+          <p className="mt-6 text-lg font-medium text-pretty text-blue-800 sm:text-xl/8">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="/"
+              className="rounded-md bg-blue-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:g-blue-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800"
+            >
+              Go back home
+            </a>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
